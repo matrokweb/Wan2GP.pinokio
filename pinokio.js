e404de71bc76ffba646211a8f4f541c220cb5567
matrokweb <matrokweb@gmail.com>
2026-01-01 23:12:43 +0100

    Add files via upload

---
+++ pinokio.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "name": "Wan2GP",
+  "description": "Fast AI Video Generation per GPU poor (Wan2.1, Hunyuan, LTV). Gradio UI su http://127.0.0.1:7860",
+  "icon": "https://github.com/deepbeepmeep/Wan2GP/blob/main/icon.png?raw=true",
+  "menu": [{
+    "text": "Guides",
+    "href": "https://github.com/deepbeepmeep/Wan2GP/blob/main/docs/INSTALLATION.md"
+  }]
+}
